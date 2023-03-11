f7b3a51a1c2d0f87ba18254c8c28bc132210dc82
Ahmed Atef <ahmedateftefa17@gmail.com>
2023-03-11 11:55:35 +0200

    Delete product.py

--- api/serializers/product.py
+++
@@ -1,9 +0,0 @@
-from rest_framework import serializers
-
-from ..models.Product import Product
-
-
-class Product(serializers.ModelSerializer):
-    class Meta:
-        model = Product
-        fields = ('name', 'price')
